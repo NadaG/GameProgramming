8274d42a4735a48c476d1f1dae514af9be6e14b2
park <wlgur01014@naver.com>
2016-04-16 14:34:43 +0900

    first commit

---
+++ main.cpp
@@ -0,0 +1,36 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: main.cpp
+////////////////////////////////////////////////////////////////////////////////
+#include "systemclass.h"
+#include<d3d10.h>
+
+#pragma comment( lib, "d3d11.lib" )   // D3D11 라이브러리 
+#pragma comment( lib, "d3dx11.lib" )
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdshow)
+{
+	SystemClass* System;
+	bool result;
+
+
+	// Create the system object.
+	System = new SystemClass;
+	if (!System)
+	{
+		return 0;
+	}
+
+	// Initialize and run the system object.
+	result = System->Initialize();
+	if (result)
+	{
+		System->Run();
+	}
+
+	// Shutdown and release the system object.
+	System->Shutdown();
+	delete System;
+	System = 0;
+
+	return 0;
+}
