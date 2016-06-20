351e9e2f6a624546dca748935ea80a205816e4a2
iyaa <iyaa.cs@gmail.com>
2016-06-21 00:56:31 +0900

    푸시 푸시 베이베

--- lightshaderclass.h
+++ lightshaderclass.h
@@ -21,57 +21,64 @@ using namespace std;
 class LightShaderClass
 {
 private:
-	struct MatrixBufferType
-	{
-		D3DXMATRIX world;
-		D3DXMATRIX view;
-		D3DXMATRIX projection;
-	};
+    struct MatrixBufferType
+    {
+	   D3DXMATRIX world;
+	   D3DXMATRIX view;
+	   D3DXMATRIX projection;
+    };
 
-	struct CameraBufferType
-	{
-		D3DXVECTOR3 cameraPosition;
-		float padding;
-	};
+    struct CameraBufferType
+    {
+	   D3DXVECTOR3 cameraPosition;
+	   float padding;
+    };
 
-	struct LightBufferType
-	{
-		D3DXVECTOR4 ambientColor;
-		D3DXVECTOR4 diffuseColor;
-		D3DXVECTOR3 lightDirection;
-		float specularPower;
-		D3DXVECTOR4 specularColor;
-		D3DXVECTOR4 pointLightColor;
-		D3DXVECTOR4 pointLight;
-	};
+    struct LightBufferType
+    {
+	   D3DXVECTOR4 ambientColor;
+	   D3DXVECTOR4 diffuseColor;
+	   D3DXVECTOR3 lightDirection;
+	   float specularPower;
+	   D3DXVECTOR4 specularColor;
+	   D3DXVECTOR4 pointLightColor1;
+	   D3DXVECTOR4 pointLight1;
+	   D3DXVECTOR4 pointLightColor2;
+	   D3DXVECTOR4 pointLight2;
+	   D3DXVECTOR4 pointLightColor3;
+	   D3DXVECTOR4 pointLight3;
+	   D3DXVECTOR4 spotLightColor;
+	   D3DXVECTOR4 spotLightDirection;
+	   D3DXVECTOR4 spotLight;
+    };
 
 public:
-	LightShaderClass();
-	LightShaderClass(const LightShaderClass&);
-	~LightShaderClass();
+    LightShaderClass();
+    LightShaderClass(const LightShaderClass&);
+    ~LightShaderClass();
 
-	bool Initialize(ID3D11Device*, HWND);
-	void Shutdown();
-	bool Render(ID3D11DeviceContext*, int, D3DXMATRIX, D3DXMATRIX, D3DXMATRIX, ID3D11ShaderResourceView*, D3DXVECTOR3, D3DXVECTOR4, D3DXVECTOR4, 
-				D3DXVECTOR3, D3DXVECTOR4, float);
-
-private:
-	bool InitializeShader(ID3D11Device*, HWND, WCHAR*, WCHAR*);
-	void ShutdownShader();
-	void OutputShaderErrorMessage(ID3D10Blob*, HWND, WCHAR*);
-
-	bool SetShaderParameters(ID3D11DeviceContext*, D3DXMATRIX, D3DXMATRIX, D3DXMATRIX, ID3D11ShaderResourceView*, D3DXVECTOR3, D3DXVECTOR4, D3DXVECTOR4,
-							 D3DXVECTOR3, D3DXVECTOR4, float);
-	void RenderShader(ID3D11DeviceContext*, int);
+    bool Initialize(ID3D11Device*, HWND);
+    void Shutdown();
+    bool Render(ID3D11DeviceContext*, int, D3DXMATRIX, D3DXMATRIX, D3DXMATRIX, ID3D11ShaderResourceView*, D3DXVECTOR3, D3DXVECTOR4, D3DXVECTOR4,
+	   D3DXVECTOR3, D3DXVECTOR4, float);
 
 private:
-	ID3D11VertexShader* m_vertexShader;
-	ID3D11PixelShader* m_pixelShader;
-	ID3D11InputLayout* m_layout;
-	ID3D11SamplerState* m_sampleState;
-	ID3D11Buffer* m_matrixBuffer;
-	ID3D11Buffer* m_cameraBuffer;
-	ID3D11Buffer* m_lightBuffer;
+    bool InitializeShader(ID3D11Device*, HWND, WCHAR*, WCHAR*);
+    void ShutdownShader();
+    void OutputShaderErrorMessage(ID3D10Blob*, HWND, WCHAR*);
+
+    bool SetShaderParameters(ID3D11DeviceContext*, D3DXMATRIX, D3DXMATRIX, D3DXMATRIX, ID3D11ShaderResourceView*, D3DXVECTOR3, D3DXVECTOR4, D3DXVECTOR4,
+	   D3DXVECTOR3, D3DXVECTOR4, float);
+    void RenderShader(ID3D11DeviceContext*, int);
+
+private:
+    ID3D11VertexShader* m_vertexShader;
+    ID3D11PixelShader* m_pixelShader;
+    ID3D11InputLayout* m_layout;
+    ID3D11SamplerState* m_sampleState;
+    ID3D11Buffer* m_matrixBuffer;
+    ID3D11Buffer* m_cameraBuffer;
+    ID3D11Buffer* m_lightBuffer;
 };
 
 #endif
